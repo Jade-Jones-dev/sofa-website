b42727d5a9748d5a2385d5dba6d7213b2866b5d4
JadeJones2022 <100792140+JadeJones2022@users.noreply.github.com>
2022-11-10 20:04:06 +0000

    refactored product js

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,5 @@
 const id = getID();
+const addButton = document.getElementById("addToCart");
 let product;
 
 //fetch data from the backend api
@@ -9,6 +10,7 @@ fetch("http://localhost:3000/api/products/" + id)
 		ListenForCartAddition(product);
 	});
 
+// display the product on the page
 function display(product) {
 	let productImage = document.querySelector(".item__img");
 	let productTitle = document.getElementById("title");
@@ -21,6 +23,7 @@ function display(product) {
 	productDescription.textContent = product.description;
 	productImage.innerHTML = `<img src=${product.imageUrl} alt=${product.altTxt}>`;
 
+	// create the options for the colors
 	let dropdownEl = document.getElementById("colors");
 	let productColors = product.colors;
 
@@ -28,17 +31,16 @@ function display(product) {
 		let optionEl = document.createElement("option");
 		optionEl.setAttribute("value", color);
 		optionEl.textContent = color;
-
 		dropdownEl.append(optionEl);
 	});
 }
 
+//function to add products to cart
 function ListenForCartAddition(product) {
-	const addButton = document.getElementById('addToCart')
 	addButton.addEventListener("click", () => {
 		const qty = Number(document.getElementById("quantity").value);
 		const color = document.getElementById("colors").value;
-
+		//check if quantity and color are valid
 		if (qty < 1 || qty > 100) {
 			alert("Please choose a quantity between 1 and 100");
 			return;
@@ -47,7 +49,7 @@ function ListenForCartAddition(product) {
 			alert("Please select a color");
 			return;
 		}
-
+		//check if cart is empty
 		const isCartEmpty = !localStorage.getItem("products");
 
 		if (isCartEmpty) {
@@ -83,6 +85,7 @@ function ListenForCartAddition(product) {
 	});
 }
 
+// get the information from home page
 function getID() {
 	const params = new Proxy(new URLSearchParams(window.location.search), {
 		get: (searchParams, prop) => searchParams.get(prop),
@@ -91,50 +94,6 @@ function getID() {
 	return params.id;
 }
 
-// next steps- add event listener to button
-// const addButton = document.getElementById("addToCart");
-// const productQuantity = document.getElementById("quantity");
 
-// // create function to check if options are available
-// function formCheck() {
-// 	let colorsDropdown = document.getElementById("colors");
-// 	if (productQuantity.value < 1) {
-// 		alert("Please choose a quantity");
-// 	}
-// 	if (colorsDropdown.value == "") {
-// 		alert("Please select a color");
-// 	}
-// }
-
-//checking if there are previous details in local storage if not add teh product to local storage
-// function cartCheck(product) {
-// 	let cart = [];
-// 	colorsDropdown = document.getElementById("colors");
-// 	let productQuantity = document.getElementById("quantity");
-// 	productTitle = document.getElementById("title");
-// 	if (localStorage.getItem("product") !== null) {
-// 		cart.push(productTitle.value, colorsDropdown.value, productQuantity.value);
-// 		window.localStorage.setItem("product", JSON.stringify(cart));
-// 	} else {
-// 		console.log(localStorage);
-// 	}
-
-// }
-
-//should I create an array of objects?
-
-// how to check items in localstorage? Do I just check the cart array??
-// next step how to check if the items in local storage already contain the product I am trying to add?
-
-//  display the product- done
-//// add an event listener - done
-// check whether quantity is valid - done
-// check whether color is valid - done
-// check whether yhere are products in local storage- need to read up on tthis https://blog.logrocket.com/localstorage-javascript-complete-guide/ - done
-// https://stackoverflow.com/questions/5410820/how-can-i-show-all-the-localstorage-saved-variables
-// if yes
-//  does the selected product and color already exist? if yes add the color and quntity
-// https://attacomsian.com/blog/web-storage-api-local-storage-session-storage
-// if no create an array - done
 
 
