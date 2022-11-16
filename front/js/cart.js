d66775917542177610ef0376e74cee5c33bfd674
JadeJones2022 <100792140+JadeJones2022@users.noreply.github.com>
2022-11-16 08:51:07 +0000

    items displaying correctly

--- front/js/cart.js
+++ front/js/cart.js
@@ -8,7 +8,8 @@ fetch('http://localhost:3000/api/products')
 .then(allProducts => {
     const cart = JSON.parse(localStorage.getItem("products"));
     const products = buildCompleteList(cart, allProducts);
-    
+    // console.log(products);
+     
 })
 // insert the elements on the page
 function buildCompleteList(cart, Allproducts){
@@ -19,45 +20,79 @@ function buildCompleteList(cart, Allproducts){
         list.push(item)
         })   
     console.log(list);
-    
+    createCartItems(list); 
+   
 }
 
-/*
-        let addEl = document.getElementById("cart__items");
+ 
+function createCartItems(list){
+  list.forEach((product) => {
+		let articleEl = document.createElement("article");
+		let imageDivEl = document.createElement("div");
+    let imageEl = document.createElement('img')
+		let cartEl = document.createElement("div");
+		let cartDivEl = document.createElement("div");
+		let nameEl = document.createElement("h2");
+		let colorEl = document.createElement("p");
+		let priceEl = document.createElement("p");
+    let cartElContent = document.createElement("div");
+		let cartElContentQuantity = document.createElement("div");
+		let cartPEl = document.createElement("p");
+    let deleteEl = document.createElement('div');
+    let deleteItemEl = document.createElement('p');
+    let itemQuantityEl = document.createElement('input')
 
-        let articleEl = document.createElement('article')
-        let imageEl = document.createElement('div')
-        let cartEl = document.createElement('div')
-        let cartDivEl = document.createElement('div')
-        let nameEl = document.createElement('h2');
-        let colorEl = document.createElement("p");
-        let priceEl = document.createElement("p");
+		articleEl.className = "cart__item";
+		imageDivEl.className = "cart__item__img";
+		cartEl.className = "cart__item__content";
+		cartDivEl.className = "cart__item__content__description";
+    cartElContent.className = "cart__item__content__settings";
+		cartElContentQuantity.className = "cart__item__content__settings__quantity";
+    deleteEl.className = "cart__item__content__settings__delete";
+    itemQuantityEl.className = "itemQuantity";
+    itemQuantityEl.name = 'itemQuantity'
 
-        articleEl.className = 'cart__item';
-        imageEl.className = "cart__item__img";
-        cartEl.className = "cart__item__content";
-        cartDivEl.className = "cart__item__content__description";
-
-        imageEl.setAttribute("src", product.imageUrl);
-				imageEl.setAttribute("alt", product.altTxt);
-        nameEl.innerText = product.name
-        colorEl.innertext = product.color
-        priceEl.innerText = product.price
-
-        cartDivEl.append(nameEl, colorEl, priceEl)
-        cartEl.append(cartDivEl)
-        articleEl.append(imageEl, cartDivEl)
-        addEl.append(cartEl)
-
+		imageEl.setAttribute("src", product.imageUrl);
+		imageEl.setAttribute("alt", product.altTxt);
+    articleEl.setAttribute("data-id", product._id);
+		articleEl.setAttribute("data-color", product.color);
+		nameEl.innerText = product.name;
+		colorEl.innerHTML = product.color;
+		priceEl.innerText = `€${product.price}`;
+		cartPEl.innerHTML = `Quantity : `;
+    deleteItemEl.innerHTML = `Delete`;
+    itemQuantityEl.setAttribute('value', product.qty);
+    itemQuantityEl.setAttribute('min', 1)
+    itemQuantityEl.setAttribute('max', 100)
 
 
-        // check if i need this
+    deleteEl.append(deleteItemEl)
+    cartElContentQuantity.append(cartPEl, itemQuantityEl)
+    cartElContent.append(cartElContentQuantity, deleteEl);
+		cartDivEl.append(nameEl, colorEl, priceEl, cartElContent);
+    imageDivEl.append(imageEl)
+		cartEl.append(cartDivEl);
+		articleEl.append(imageDivEl, cartEl);
+		cartItems.append(articleEl);
 
-        // let cartElContent = document.createElement('div');
-        // let cartElContentQuantity = document.createElement("div");
-        // let cartPEl = document.createElement("p");
+		// check if i need this
 
-*/
+		
+
+    
+	})
+}
+
+{/* <div class="cart__item__content__settings"> done
+                    <div class="cart__item__content__settings__quantity"> done
+                      <p>Quantity : </p> done
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                      <p class="deleteItem">Delete</p>
+                    </div>
+                  </div> */}
+
 
 /*
              <!--  <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
